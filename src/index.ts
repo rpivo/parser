418d6bda636f3fc7d4e17832f4260d471d6abfc6
Ryan Pivovar <ryanpivovar@gmail.com>
2020-06-04 19:41:31 -0400

    print words separated by whitespace

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import fs from 'fs';
+
+const code = fs.readFileSync('./bin/example.js', 'utf-8');
+
+let it = 0;
+let letterSequence = [];
+
+while (it < code.length) {
+  const char = code[it];
+
+  const isLetter = /^[a-zA-Z]+$/.test(char);
+  const isWhitespace = /\s/.test(char);
+
+  if (isLetter) letterSequence.push(char);
+  if (isWhitespace && letterSequence) {
+    const word = letterSequence.join('');
+    console.log('word', word);
+    letterSequence = [];
+  };
+
+  it++;
+}
+
+const ast = {
+  'type': 'Program',
+  'start': 0,
+  'end': code.length,
+  'body': [],
+  'sourceType': 'module',
+};
