a36a52fc80dfcbd783f93e58965d4c922ab11435
Ryan Pivovar <ryanpivovar@gmail.com>
2020-06-07 15:56:41 -0400

    add more reserved words to reserverdWords.ts

--- bin/reservedWords.js
+++ bin/reservedWords.js
@@ -1,6 +1,25 @@
+export var ControlFlow;
+(function (ControlFlow) {
+    ControlFlow["break"] = "break";
+    ControlFlow["case"] = "case";
+    ControlFlow["continue"] = "continue";
+    ControlFlow["default"] = "default";
+})(ControlFlow || (ControlFlow = {}));
+export var ErrorHandling;
+(function (ErrorHandling) {
+    ErrorHandling["catch"] = "catch";
+    ErrorHandling["debugger"] = "debugger";
+})(ErrorHandling || (ErrorHandling = {}));
 export var Variables;
 (function (Variables) {
+    Variables["class"] = "class";
     Variables["const"] = "const";
     Variables["let"] = "let";
     Variables["var"] = "var";
 })(Variables || (Variables = {}));
+export var WordTypes;
+(function (WordTypes) {
+    WordTypes["ControlFlow"] = "ControlFlow";
+    WordTypes["ErrorHandling"] = "ErrorHandling";
+    WordTypes["Variables"] = "Variables";
+})(WordTypes || (WordTypes = {}));
--- src/reservedWords.ts
+++ src/reservedWords.ts
@@ -1,5 +1,24 @@
+export enum ControlFlow {
+  break = 'break',
+  case = 'case',
+  continue = 'continue',
+  default = 'default',
+}
+
+export enum ErrorHandling {
+  catch = 'catch',
+  debugger = 'debugger',
+}
+
 export enum Variables {
+  class = 'class',
   const = 'const',
   let = 'let',
   var = 'var',
+}
+
+export enum WordTypes {
+  ControlFlow = 'ControlFlow',
+  ErrorHandling = 'ErrorHandling',
+  Variables = 'Variables',
 }
