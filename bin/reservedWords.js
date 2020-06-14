d50b68fdd65dcc676d19abdc98d2688db5d5775c
Ryan Pivovar <ryanpivovar@gmail.com>
2020-06-14 17:59:07 -0400

    update reservedWords

--- bin/reservedWords.js
+++ bin/reservedWords.js
@@ -2,29 +2,45 @@ export var ControlFlow;
 (function (ControlFlow) {
     ControlFlow["break"] = "break";
     ControlFlow["case"] = "case";
+    ControlFlow["catch"] = "catch";
     ControlFlow["continue"] = "continue";
     ControlFlow["default"] = "default";
     ControlFlow["do"] = "do";
     ControlFlow["else"] = "else";
+    ControlFlow["finally"] = "finally";
+    ControlFlow["for"] = "for";
+    ControlFlow["if"] = "if";
+    ControlFlow["in"] = "in";
+    ControlFlow["return"] = "return";
+    ControlFlow["switch"] = "switch";
+    ControlFlow["throw"] = "throw";
+    ControlFlow["try"] = "try";
+    ControlFlow["while"] = "while";
 })(ControlFlow || (ControlFlow = {}));
-export var ErrorHandling;
-(function (ErrorHandling) {
-    ErrorHandling["catch"] = "catch";
-    ErrorHandling["debugger"] = "debugger";
-})(ErrorHandling || (ErrorHandling = {}));
 export var Variables;
 (function (Variables) {
     Variables["class"] = "class";
     Variables["const"] = "const";
+    Variables["function"] = "function";
     Variables["let"] = "let";
     Variables["var"] = "var";
 })(Variables || (Variables = {}));
 export var WordTypes;
 (function (WordTypes) {
     WordTypes["ControlFlow"] = "ControlFlow";
-    WordTypes["ErrorHandling"] = "ErrorHandling";
     WordTypes["Variables"] = "Variables";
 })(WordTypes || (WordTypes = {}));
 // unsure where to put these:
+// debugger
 // delete
 // export
+// extends
+// import
+// instanceof
+// new
+// super
+// this
+// typeof
+// void
+// with
+// yield
--- src/reservedWords.ts
+++ src/reservedWords.ts
@@ -1,30 +1,47 @@
 export enum ControlFlow {
   break = 'break',
   case = 'case',
+  catch = 'catch',
   continue = 'continue',
   default = 'default',
   do = 'do',
   else = 'else',
-}
-
-export enum ErrorHandling {
-  catch = 'catch',
-  debugger = 'debugger',
+  finally = 'finally',
+  for = 'for',
+  if = 'if',
+  in = 'in',
+  return = 'return',
+  switch = 'switch',
+  throw = 'throw',
+  try = 'try',
+  while = 'while',
 }
 
 export enum Variables {
   class = 'class',
   const = 'const',
+  function = 'function',
   let = 'let',
   var = 'var',
 }
 
 export enum WordTypes {
   ControlFlow = 'ControlFlow',
-  ErrorHandling = 'ErrorHandling',
   Variables = 'Variables',
 }
 
 // unsure where to put these:
+
+// debugger
 // delete
 // export
+// extends
+// import
+// instanceof
+// new
+// super
+// this
+// typeof
+// void
+// with
+// yield
